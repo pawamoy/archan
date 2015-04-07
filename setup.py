d1c8f6f73cc483ea782e6089c7b3f071714fc5c6
Timothée Mazzucotelli <timothee.mazzucotelli@gmail.com>
2015-04-07 16:23:55 +0200

    Ready for version 0.0.3 with universal wheel

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='archan',
-    version='0.0.2',
+    version='0.0.3',
     packages=['archan'],
     license='MPL 2.0',
 
