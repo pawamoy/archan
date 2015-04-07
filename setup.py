a7b580d76c38bbef02838d6c170eb349cba9fa24
Timothée Mazzucotelli <timothee.mazzucotelli@gmail.com>
2015-04-07 16:24:48 +0200

    Using setuptools instead of distutils

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import os
-from distutils.core import setup
+from setuptools import setup
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
