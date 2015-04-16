b92ae0784f680dbfea07067650a221aba0adea15
Timothée Mazzucotelli <timothee.mazzucotelli@gmail.com>
2015-04-16 18:13:39 +0200

    Added contents in init

--- archan/__init__.py
+++ archan/__init__.py
@@ -6,10 +6,8 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-"""
-Created on 8 janv. 2015
-
-@author: Pierre.Parrend
-"""
-
 from __future__ import unicode_literals
+
+__author__ = 'Pierre Parrend <parrend@unistra.fr>'
+__version__ = '0.0.3'
+
