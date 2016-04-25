185cd0145c14647dc4470d8bb5190d01529b8ec1
Timothée Mazzucotelli <timothee.mazzucotelli@gmail.com>
2016-04-25 18:12:19 +0200

    Update Archan and DSM

--- archan/checker.py
+++ archan/checker.py
@@ -24,15 +24,81 @@ from archan.errors import ArchanError
 
 
 class Archan(object):
-    """Architecture analyser class.
-    """
+    """Architecture analyser class."""
 
-    check_complete_mediation_implemented = True
-    check_economy_of_mechanism_implemented = True
-    check_separation_of_privileges_implemented = False
-    check_least_privileges_implemented = False
-    check_least_common_mechanism_implemented = True
-    check_layered_architecture_implemented = True
+    COMPLETE_MEDIATION = 1
+    ECONOMY_OF_MECHANISM = 2
+    SEPARATION_OF_PRIVILEGES = 4
+    LEAST_PRIVILEGES = 8
+    LEAST_COMMON_MECHANISM = 16
+    LAYERED_ARCHITECTURE = 32
+    OPEN_DESIGN = 64
+    CODE_CLEAN = 128
+
+    FAILED = 0
+    PASSED = 1
+    IGNORED = 2
+    NOT_IMPLEMENTED = 3
+
+    FLAGS = (
+        COMPLETE_MEDIATION,
+        ECONOMY_OF_MECHANISM,
+        SEPARATION_OF_PRIVILEGES,
+        LEAST_PRIVILEGES,
+        LEAST_COMMON_MECHANISM,
+        LAYERED_ARCHITECTURE,
+        OPEN_DESIGN,
+        CODE_CLEAN,
+    )
+
+    def __init__(self, dsm, flags=0,
+                 simplicity_factor=2,
+                 independence_factor=5):
+        self.dsm = dsm
+        self.flags = flags
+        self.simplicity_factor = simplicity_factor
+        self.independence_factor = independence_factor
+
+    def flag_name(self, flag):
+        if flag == Archan.COMPLETE_MEDIATION:
+            return 'Complete Mediation'
+        if flag == Archan.ECONOMY_OF_MECHANISM:
+            return 'Economy of Mechanism'
+        if flag == Archan.SEPARATION_OF_PRIVILEGES:
+            return 'Separation Of Privileges'
+        if flag == Archan.LEAST_PRIVILEGES:
+            return 'Least Privileges'
+        if flag == Archan.LEAST_COMMON_MECHANISM:
+            return 'Least Common Mechanism'
+        if flag == Archan.LAYERED_ARCHITECTURE:
+            return 'Layered Architecture'
+        if flag == Archan.OPEN_DESIGN:
+            return 'Open Design'
+        if flag == Archan.CODE_CLEAN:
+            return 'Code Clean'
+
+    def checker(self, flag):
+        if flag == Archan.COMPLETE_MEDIATION:
+            return self.complete_mediation
+        if flag == Archan.ECONOMY_OF_MECHANISM:
+            return self.economy_of_mechanism
+        if flag == Archan.LEAST_COMMON_MECHANISM:
+            return self.least_common_mechanism
+        if flag == Archan.LAYERED_ARCHITECTURE:
+            return self.layered_architecture
+        if flag == Archan.OPEN_DESIGN:
+            return self.open_design
+
+        return lambda: Archan.NOT_IMPLEMENTED
+
+    def check(self):
+        return {
+            flag:
+                self.checker(flag)()
+                if self.flags & flag
+                else Archan.IGNORED
+            for flag in Archan.FLAGS
+        }
 
     # Rules for mediation matrix generation:
     #
@@ -55,87 +121,80 @@ class Archan(object):
     #   Dependencies to data are tolerated.
     # - Data have no dependencies at all
     #   (but framework/libs would be tolerated).
-    @staticmethod
-    def _generate_mediation_matrix(dsm):
-        """Generate the mediation matrix of the given matrix.
+    def _generate_mediation_matrix(self):
+        """Generate the mediation matrix of the given matrix."""
 
-        :type dsm: :class:`DesignStructureMatrix`
-        :param dsm: matrix to generate mediation matrix from
-        :return: mediation matrix of dsm
-        """
-
-        cat = dsm.categories
-        ent = dsm.entities
-        size = dsm.size
+        cat = self.dsm.categories
+        ent = self.dsm.entities
+        size = self.dsm.size
         packages = [e.split('.')[0] for e in ent]
 
         # define and initialize the mediation matrix
-        mediation_matrix = [[0 for x in range(size)]
-                            for x in range(size)]
+        mediation_matrix = [[0 for _ in range(size)]
+                            for _ in range(size)]
 
         for i in range(0, size):
             for j in range(0, size):
-                if cat[i] == DesignStructureMatrix.framework:
-                    if cat[j] == DesignStructureMatrix.framework:
+                if cat[i] == self.dsm.framework:
+                    if cat[j] == self.dsm.framework:
                         mediation_matrix[i][j] = -1
                     else:
                         mediation_matrix[i][j] = 0
-                elif cat[i] == DesignStructureMatrix.core_lib:
-                    if (cat[j] == DesignStructureMatrix.framework or
-                            cat[j] == DesignStructureMatrix.core_lib or
+                elif cat[i] == self.dsm.core_lib:
+                    if (cat[j] == self.dsm.framework or
+                            cat[j] == self.dsm.core_lib or
                             ent[i].startswith(packages[j] + '.') or
                             i == j):
                         mediation_matrix[i][j] = -1
                     else:
                         mediation_matrix[i][j] = 0
-                elif cat[i] == DesignStructureMatrix.app_lib:
-                    if (cat[j] == DesignStructureMatrix.framework or
-                            cat[j] == DesignStructureMatrix.core_lib or
-                            cat[j] == DesignStructureMatrix.app_lib or
+                elif cat[i] == self.dsm.app_lib:
+                    if (cat[j] == self.dsm.framework or
+                            cat[j] == self.dsm.core_lib or
+                            cat[j] == self.dsm.app_lib or
                             ent[i].startswith(packages[j] + '.') or
                             i == j):
                         mediation_matrix[i][j] = -1
                     else:
                         mediation_matrix[i][j] = 0
-                elif cat[i] == DesignStructureMatrix.app_module:
+                elif cat[i] == self.dsm.app_module:
                     # we cannot force an app module to import things from
                     # the broker if the broker itself did not import anything
-                    if (cat[j] == DesignStructureMatrix.framework or
-                            cat[j] == DesignStructureMatrix.core_lib or
-                            cat[j] == DesignStructureMatrix.app_lib or
-                            cat[j] == DesignStructureMatrix.broker or
-                            cat[j] == DesignStructureMatrix.data or
+                    if (cat[j] == self.dsm.framework or
+                            cat[j] == self.dsm.core_lib or
+                            cat[j] == self.dsm.app_lib or
+                            cat[j] == self.dsm.broker or
+                            cat[j] == self.dsm.data or
                             ent[i].startswith(packages[j] + '.') or
                             i == j):
                         mediation_matrix[i][j] = -1
                     else:
                         mediation_matrix[i][j] = 0
-                elif cat[i] == DesignStructureMatrix.broker:
+                elif cat[i] == self.dsm.broker:
                     # we cannot force the broker to import things from
                     # app modules if there is nothing to be imported
-                    if (cat[j] == DesignStructureMatrix.app_module or
-                            cat[j] == DesignStructureMatrix.framework or
+                    if (cat[j] == self.dsm.app_module or
+                            cat[j] == self.dsm.framework or
                             ent[i].startswith(packages[j] + '.') or
                             i == j):
                         mediation_matrix[i][j] = -1
                     else:
                         mediation_matrix[i][j] = 0
-                elif cat[i] == DesignStructureMatrix.data:
-                    if (cat[j] == DesignStructureMatrix.framework or
+                elif cat[i] == self.dsm.data:
+                    if (cat[j] == self.dsm.framework or
                             i == j):
                         mediation_matrix[i][j] = -1
                     else:
                         mediation_matrix[i][j] = 0
                 else:
-                    mediation_matrix[i][j] = -2  # errors in the generation
+                    # mediation_matrix[i][j] = -2  # errors in the generation
                     raise ArchanError(
-                        "Mediation matrix value NOT generated for %s:%s" % (
+                        'Mediation matrix value NOT generated for %s:%s' % (
                             i, j))
 
         return mediation_matrix
 
-    @staticmethod
-    def _check_matrices_compliance(dsm, complete_mediation_matrix):
+    def _matrices_compliance(self, complete_mediation_matrix):
         """Check if matrix and its mediation matrix are compliant.
 
         :type dsm: :class:`DesignStructureMatrix`
@@ -145,7 +204,7 @@ class Archan(object):
         :return: bool, True if compliant, else False
         """
 
-        matrix = dsm.dependency_matrix
+        matrix = self.dsm.dependency_matrix
         rows_dep_matrix = len(matrix)
         cols_dep_matrix = len(matrix[0])
         rows_med_matrix = len(complete_mediation_matrix)
@@ -167,14 +226,13 @@ class Archan(object):
                     discrepancy_found = True
                     print("Matrix discrepancy found "
                           "at %s:%s (%s:%s): %s/%s" % (
-                              i, j, dsm.entities[i], dsm.entities[j],
+                              i, j, self.dsm.entities[i], self.dsm.entities[j],
                               complete_mediation_matrix[i][j],
                               matrix[i][j]))
 
         return not discrepancy_found
 
-    @staticmethod
-    def check_complete_mediation(dsm):
+    def complete_mediation(self):
         """Check if matrix and its mediation matrix are compliant, meaning
         that number of dependencies for each (line, column) is either 0 if
         the mediation matrix (line, column) is 0, or >0 if the mediation matrix
@@ -186,13 +244,12 @@ class Archan(object):
         """
 
         # generate complete_mediation_matrix according to each category
-        med_matrix = Archan._generate_mediation_matrix(dsm)
-        matrices_compliant = Archan._check_matrices_compliance(dsm, med_matrix)
+        med_matrix = Archan._generate_mediation_matrix(self.dsm)
+        matrices_compliant = Archan._matrices_compliance(self.dsm, med_matrix)
         # check comparison result
         return matrices_compliant
 
-    @staticmethod
-    def check_economy_of_mechanism(dsm, simplicity_factor=2):
+    def economy_of_mechanism(self):
         """Check economy of mechanism.
 
         As first abstraction, number of dependencies between two modules
@@ -206,43 +263,28 @@ class Archan(object):
 
         # economy_of_mechanism
         economy_of_mechanism = False
-        dependency_matrix = dsm.dependency_matrix
-        categories = dsm.categories
-        dsm_size = dsm.size
+        dependency_matrix = self.dsm.dependency_matrix
+        categories = self.dsm.categories
+        dsm_size = self.dsm.size
 
         dependency_number = 0
         # evaluate Matrix(dependency_matrix)
         for i in range(0, dsm_size):
             for j in range(0, dsm_size):
-                if (categories[i] != DesignStructureMatrix.framework and
-                        categories[j] != DesignStructureMatrix.framework and
+                if (categories[i] != self.dsm.framework and
+                        categories[j] != self.dsm.framework and
                         dependency_matrix[i][j] > 0):
                     dependency_number += 1
                     # check comparison result
-        if dependency_number < dsm_size * simplicity_factor:
+        if dependency_number < dsm_size * self.simplicity_factor:
             economy_of_mechanism = True
         else:
             print("dependency_number: %s" % dependency_number)
             print("rowsdep_matrix: %s" % dsm_size)
-            print("expected dependencies: %s" % simplicity_factor)
+            print("expected dependencies: %s" % self.simplicity_factor)
         return economy_of_mechanism
 
-    @staticmethod
-    def check_separation_of_privileges(dsm):
-        # separation_of_privileges_matrix
-        separation_of_privileges = False
-        # check comparison result
-        return separation_of_privileges
-
-    @staticmethod
-    def check_least_privileges(dsm):
-        # least_privileges_matrix
-        least_privileges = False
-        # check comparison result
-        return least_privileges
-
-    @staticmethod
-    def check_least_common_mechanism(dsm, independence_factor=5):
+    def least_common_mechanism(self):
         """Check least common mechanism.
 
         :type dsm: :class:`DesignStructureMatrix`
@@ -253,39 +295,38 @@ class Archan(object):
         # leastCommonMechanismMatrix
         least_common_mechanism = False
         # get the list of dependent modules for each module
-        dependency_matrix = dsm.dependency_matrix
-        categories = dsm.categories
-        dsm_size = dsm.size
+        dependency_matrix = self.dsm.dependency_matrix
+        categories = self.dsm.categories
+        dsm_size = self.dsm.size
 
         dependent_module_number = []
         # evaluate Matrix(dependency_matrix)
         for j in range(0, dsm_size):
             dependent_module_number.append(0)
             for i in range(0, dsm_size):
-                if (categories[i] != DesignStructureMatrix.framework and
-                        categories[j] != DesignStructureMatrix.framework and
+                if (categories[i] != self.dsm.framework and
+                        categories[j] != self.dsm.framework and
                         dependency_matrix[i][j] > 0):
                     dependent_module_number[j] += 1
         # except for the broker if any  and libs, check that threshold is not
         # overlapped
         #  index of brokers
         #  and app_libs are set to 0
-        for index, item in enumerate(dsm.categories):
-            if (item == DesignStructureMatrix.broker or
-                    item == DesignStructureMatrix.app_lib):
+        for index, item in enumerate(self.dsm.categories):
+            if (item == self.dsm.broker or
+                    item == self.dsm.app_lib):
                 dependent_module_number[index] = 0
-        if max(dependent_module_number) <= old_div(dsm.size, independence_factor):  # noqa
+        if max(dependent_module_number) <= old_div(self.dsm.size, self.independence_factor):  # noqa
             least_common_mechanism = True
         else:
             print('max number of dependencies to a module: %s' %
                   max(dependent_module_number))
             print('max number of expected dependencies: %s' %
-                  int(old_div(dsm.size, independence_factor)))
+                  int(old_div(self.dsm.size, self.independence_factor)))
 
         return least_common_mechanism
 
-    @staticmethod
-    def check_layered_architecture(dsm):
+    def layered_architecture(self):
         """Check layered architecture.
 
         :type dsm: :class:`DesignStructureMatrix`
@@ -293,43 +334,22 @@ class Archan(object):
         :return: bool
         """
 
-        for i in range(0, dsm.size - 1):
-            for j in range(i + 1, dsm.size):
-                if (dsm.categories[i] != DesignStructureMatrix.broker and
-                        dsm.categories[j] != DesignStructureMatrix.broker):
-                    if dsm.dependency_matrix[i][j] > 0:
+        for i in range(0, self.dsm.size - 1):
+            for j in range(i + 1, self.dsm.size):
+                if (self.dsm.categories[i] != self.dsm.broker and
+                            self.dsm.categories[j] != self.dsm.broker):
+                    if self.dsm.dependency_matrix[i][j] > 0:
                         return False
         return True
 
-    @staticmethod
-    def check_open_design():
+    def open_design(self):
         """Check if all criteria checking methods are implemented.
 
         :return: bool, True if all methods are implemented, else False
         """
 
         # check that compliance with secure design principles are performed
-        open_design = (Archan.check_complete_mediation_implemented and
-                       Archan.check_economy_of_mechanism_implemented and
-                       Archan.check_separation_of_privileges_implemented and
-                       Archan.check_least_privileges_implemented and
-                       Archan.check_least_common_mechanism_implemented and
-                       Archan.check_layered_architecture_implemented)
+        open_design = all(self.checker(flag).__name__ != (lambda: 0).__name__
+                          for flag in Archan.FLAGS)
         return open_design
 
-    @staticmethod
-    def check_code_clean():
-        # TODO: pyflakes and mccabe for app_modules ?
-        # or reuse the summary file of the previous lint
-        return True
-
-    @staticmethod
-    def check_all(dsm):
-        return {'CM': Archan.check_complete_mediation(dsm),
-                'EOM': Archan.check_economy_of_mechanism(dsm),
-                'SOP': Archan.check_separation_of_privileges(dsm),
-                'LP': Archan.check_least_privileges(dsm),
-                'LCM': Archan.check_least_common_mechanism(dsm),
-                'LA': Archan.check_layered_architecture(dsm),
-                'OD': Archan.check_open_design(),
-                'CC': Archan.check_code_clean()}
--- archan/dsm.py
+++ archan/dsm.py
@@ -19,36 +19,49 @@ from archan.errors import DSMError
 
 
 class DesignStructureMatrix(object):
-    """Design Structure Matrix class.
-    """
+    """Design Structure Matrix class."""
 
-    # class variables
-    framework = 'framework'
-    app_module = 'app_module'
-    core_lib = 'core_lib'
-    app_lib = 'app_lib'
-    broker = 'broker'
-    data = 'data'
-
-    def __init__(self, categories, entities, dependency_matrix):
+    def __init__(self, categories, entities, dependency_matrix,
+                 framework='framework',
+                 core_lib='core_lib',
+                 app_lib='app_lib',
+                 app_module='app_module',
+                 broker='broker',
+                 data='data'):
         # TODO - DSM: check compliance with DSM definitions and uses
         self.categories = categories
         self.entities = entities
         self.dependency_matrix = dependency_matrix
         self.size = len(dependency_matrix)
 
+        self.framework = framework
+        self.core_lib = core_lib
+        self.app_lib = app_lib
+        self.app_module = app_module
+        self.broker = broker
+        self.data = data
+
         cat_nb = len(categories)
         ent_nb = len(entities)
         rows = self.size
         columns = len(dependency_matrix[0])
+        categories_names = (framework, core_lib, app_lib,
+                            app_module, broker, data)
         if cat_nb != ent_nb:
             raise DSMError(
-                "Beware: nb of categories: %s; nb of entities: %s" % (
+                'Beware: nb of categories: %s != nb of entities: %s' % (
                     cat_nb, ent_nb))
         if rows != columns:
             raise DSMError(
-                "Beware: nb of rows: %s; nb of columns: %s in DSM matrix" % (
+                'Beware: nb of rows: %s != nb of columns: %s in DSM matrix' % (
                     rows, columns))
         if ent_nb != rows:
             raise DSMError(
-                "Beware: nb of entities: %s; nb of rows: %s" % (ent_nb, rows))
+                'Beware: nb of entities: %s != nb of rows: %s' % (
+                    ent_nb, rows))
+        for i, category in enumerate(categories):
+            if category not in categories_names:
+                raise DSMError(
+                    'Beware: category %s (in position %d) does not match '
+                    'any valid category name (%s)' % (
+                        category, i, ', '.join(categories_names)))
