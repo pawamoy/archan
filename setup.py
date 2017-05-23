72f52b2c6a29d0ef91fe720bb524825372798e19
Timothée Mazzucotelli <timothee.mazzucotelli@gmail.com>
2017-05-23 18:45:54 +0200

    Update cookiecutter branch

--- setup.py
+++ setup.py
@@ -70,4 +70,9 @@ setup(
         #   'rst': ['docutils>=0.11'],
         #   ':python_version=="2.6"': ['argparse'],
     },
+    entry_points={
+        'console_scripts': [
+            'archan = archan.cli:main',
+        ]
+    },
 )
--- tests/test_archan.py
+++ tests/test_archan.py
@@ -3,9 +3,8 @@
 """Main test script."""
 
 
-
-import archan
+from archan.cli import main
 
 def test_main():
     """Main test method."""
-    assert archan  # use your library here
+    main([])
